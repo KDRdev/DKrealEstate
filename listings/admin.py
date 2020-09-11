f81966f7771da739fe82045b2075fd22ed0b930f
dkdev <dmitry.kashuba.cv@gmail.com>
2020-09-11 14:45:04 +0200

    Updated the listing, realtors admin list view

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from listings.models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('is_published', 'realtor')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'state', 'zipcode', 'city')
+    list_per_page = 20
+
+
+admin.site.register(Listing, ListingAdmin)
