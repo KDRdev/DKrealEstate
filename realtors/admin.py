8e4d7d0e1aa1ab5b010d66b96b69bf6cdfa82c34
dkdev <dmitry.kashuba.cv@gmail.com>
2020-09-12 13:17:10 +0200

    Added is_mvp field to realtors admin view

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,7 +3,7 @@ from realtors.models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'email', 'hire_date', 'is_mvp')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_per_page = 20
