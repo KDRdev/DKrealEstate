2cacfeee4bae177059aa795d73f53043dedeb01d
dkdev <dmitry.kashuba.cv@gmail.com>
2020-09-13 14:18:47 +0200

    Creates accounts app

--- dkre/settings.py
+++ dkre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'states.apps.StatesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
